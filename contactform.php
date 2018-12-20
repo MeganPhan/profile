1e4b0e05d4ec206b75a25a182a9e689659ed5fe5
MeganPhan <kimhoang7994@gmail.com>
2018-12-20 00:08:33 -0600

    change

--- contactform.php
+++ contactform.php
@@ -1,16 +1,1 @@
-<?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
 
-    $email_subject = "New Message from Your Website";
-    $email_body = "From:  $name.\n".
-                    "Email: $email.\n".
-                        "Message: $message.\n";
-
-    $email_to = "hoang.phan@tcu.edu";
-    $header = "From: $name \r\n";
-    $header = "Reply-To: $email \r\n";
-
-    mail($email_to, $email_subject, $email_body, $header);
-    header("Location: contact.html");
